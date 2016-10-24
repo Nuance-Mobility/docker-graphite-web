cea18ea88fb71bc93b5ddbfb80b36184bde9a40c
Pete Lyons <peter.lyons@nuance.com>
2016-10-24 12:56:41 -0400

    Removed stale apache2.pid

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ -a /var/run/apache2/apache2.pid ]
+then 
+	rm /var/run/apache2/apache2.pid
+fi
+
 if [ "$1" = 'graphiteweb' ]; then
 	chmod 777 /graphite_web_init_script.sh
     ./graphite_web_init_script.sh
